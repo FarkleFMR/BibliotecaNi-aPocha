b84073e210c8adece856fe03334b5b10e4f18185
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2021-09-29 17:37:54 -0600

    Actualización
    La interfaz empezada, falta agregar más interfaz y logica

--- Libreria Niña Pochita.py
+++ Libreria Niña Pochita.py
@@ -1,12 +1,46 @@
-LibroDB = [["Mamita Yunai",["Carlos Luis Fallas"],"Soley y Valverde",7,True,True],
-           ["La Nariz",["Nikolái Gógol"],"Gadir",12,False,True],
-           ["El lenguaje de programación C",["Brian Kernighan","Dennis Ritchie"],"Prentice Hall",2,False,True]]
-ClienteDB = [["JenaMuri","Jenaro","Murillo","jenaromurilloaldecoba@gmail.com","24/09/1997"],
-             ["JoseAcu","Jose","Acuña","joseacunacarrera@gmail.com","27/6/1999"]]
+from tkinter import *
+from tkinter import ttk
+import tkinter as tk
 
+class Autor:
+    def __init__(self,nombre,apellido,nacionalidad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.nacionalidad = nacionalidad
+
+class Libro:
+    def __init__(self,titulo,autor,editorial,ediccion,prestado):
+        self.titulo = titulo
+        self.autor = autor
+        self.editorial = editorial
+        self.ediccion = ediccion
+        self.prestado = prestado
+        self.activo = True
+
+class Cliente:
+    def __init__(self,username,nombre,apellido,email,fechaNacimiento):
+        self.username = username
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.fechaNacimiento = fechaNacimiento
+
+a1 = Autor("Carlos Luis","Fallas","Costarricense")
+a2 = Autor("Nikolái","Gógol","Ucraniano")
+a3 = Autor("Brian","Kernighan","Canadiense")
+a4 = Autor("Dennis","Ritchie","Estadounidense")
+
+l1 = Libro("Mamita Yunai",[a1],"Soley y Valverde",7,False)
+l2 = Libro("La Nariz",[a2],"Gadir",12,False)
+l3 = Libro("El lenguaje de programación C",[a3,a4],"Prentice Hall",2,False)
+
+c1 = Cliente("JenaMuri","Jenaro","Murillo","jenaromurilloaldecoba@gmail.com","24/09/1997")
+c2 = Cliente("JoseAcu","Jose","Acuña","joseacunacarrera@gmail.com","27/6/1999")
+LibroDB = [l1,l2,l3]
+ClienteDB = [c1,c2]
 
 class Prestamo:
-    def _init_(self,Id,Username,Libros,FechaPrestamo,FechaDevolucion,Activo):
+    def __init__(self,Id,Username,Libros,FechaPrestamo,FechaDevolucion,Activo):
         self.Id = Id
         self.Username = Username
         self.Libros = Libros
@@ -15,7 +49,7 @@ class Prestamo:
         self.Activo = Activo
 
 class Devolucion:
-    def _init_(self,Id,Username,Libros,FechaDevolucion):
+    def __init__(self,Id,Username,Libros,FechaDevolucion):
         self.Id = Id
         self.Username = Username
         self.Libros = Libros
@@ -38,6 +72,7 @@ class RegistrarPrestamosUI:
         print("Error")
 
 class RegistrarPrestamoController:
+        
     def mostrarLibros(self):
         print("Muestra los libros disponibles")
     def validaDatos(self,prestamo):
@@ -52,6 +87,7 @@ class LibreriaDB:
         print("Devuelve la info del prestamo")
 
 class RegistrarDevUI:
+        
     def registrarDevolucion(self):
         print("Crear devolucion")
     def insertarInfo(self,idPedido,fechaDev):
@@ -64,8 +100,43 @@ class RegistrarDevUI:
         print("Se ha ejecutado con exito")
     def enviarError(Self):
         print("Error")
+        
 class RegistrarDevolucionController:
     def consultarInfo(self,idPedido,fechaDev):
         print("Va a buscar el pedido")
         
-    
+class MenuUI(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.grid()
+        self.create_widgets()
+    def create_widgets(self):
+        self.titulo = tk.Label(self)
+        self.titulo["text"] = "Libreria Niña Pochita"
+        self.titulo["font"] = 30
+        self.titulo.pack(side="top", padx=20,pady=10)
+        self.gestlibros = tk.Button(self)
+        self.gestlibros["text"] = "Gestionar Libros"
+        self.gestlibros["width"] = 15
+        self.gestlibros.pack(side="top", pady=10)
+        self.registp = tk.Button(self)
+        self.registp["text"] = "Registrar Préstamo"
+        self.registp["width"] = 15
+        self.registp.pack(side="top", pady=10)
+        self.registd = tk.Button(self)
+        self.registd["text"] = "Registrar Devolución"
+        self.registd["width"] = 15
+        self.registd.pack(side="top", pady=10)
+        self.verlibros = tk.Button(self)
+        self.verlibros["text"] = "Ver Libros"
+        self.verlibros["width"] = 15
+        self.verlibros.pack(side="top", pady=10)
+
+class MenuController:
+    root = Tk()
+    app = MenuUI(master = root)
+    app.mainloop()
+MenuController()
+
+
